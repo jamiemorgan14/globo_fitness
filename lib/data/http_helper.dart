0cb325117f7208a9dd21c793ac875b4f00e2f834
JamieMorgan <jmorgan3475@gmail.com>
2021-10-10 19:09:14 -0600

    weather app

---
+++ lib/data/http_helper.dart
@@ -0,0 +1,18 @@
+import 'package:globo_fitness/data/weather.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HttpHelper {
+  final String authority = 'api.openweathermap.org';
+  final String path = 'data/2.5/weather';
+  final String apiKey = '3977b037300bbdede1eb3920bb65114b';
+
+  Future<Weather> getWeather(String location) async {
+    Map<String, dynamic> parameters = {'q': location, 'appId': apiKey};
+    Uri uri = Uri.https(authority, path, parameters);
+    http.Response result = await http.get(uri);
+    Map<String, dynamic> data = json.decode(result.body);
+    Weather weather = Weather.fromJson(data);
+    return weather;
+  }
+}
